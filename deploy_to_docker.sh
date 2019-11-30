abd37037dd6f8969a3183a9fdec9b8469d3ce6ec
talik.aziiizi <talik.aziiizi@gmail.com>
2019-11-30 18:27:59 -0500

    yarn.lock

--- deploy_to_docker.sh
+++ deploy_to_docker.sh
@@ -8,13 +8,17 @@ docker stop vendorforest-demov1.0
 docker rm vendorforest-demov1.0
 docker rmi -f vendorforest/demov1.0 
 
+docker stop vendorforestapp-build
+docker rm vendorforestapp-build
+docker rmi -f vendorforestapp-build 
+
 #build
 # echo "app dependencies."
-# docker build -t vendorforestapp-build -f Dockerfile.build . 
+docker build -t vendorforestapp-build -f Dockerfile.build . 
 
 #deploy app resources
-# docker run vendorforestapp-build > node_modules.tar.gz
-# yarn install --frozen-lockfile --production;
+docker run vendorforestapp-build > node_modules.tar.gz
+yarn install --frozen-lockfile --production;
 
 docker build -t vendorforest/demov1.0 -f Dockerfile.dist .
 
